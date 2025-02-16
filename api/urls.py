efa21875a98d88255fcb1b7fe21d5597ec6567f6
Aftab Hussain <afthabhussain33@gmail.com>
2025-02-15 19:51:58 -0600

    Updated api/Urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .views import home
+from .views import generate_diet
 
 urlpatterns = [
-    path('', home),
+    path("generate-diet/", generate_diet, name="generate_diet"),
 ]
